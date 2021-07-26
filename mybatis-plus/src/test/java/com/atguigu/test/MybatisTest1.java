0d255569d93b024bb7d8175024be736e9685995b
harry <harry@atguigu.com>
2021-07-26 19:10:33 +0800

    Initial commit

---
+++ mybatis-plus/src/test/java/com/atguigu/test/MybatisTest1.java
@@ -0,0 +1,19 @@
+package com.atguigu.test;
+
+import com.atguigu.mybatisplus.pojo.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MybatisTest1 {
+
+    @Autowired
+    private User user;
+
+
+    @Test
+    public void test01(){
+
+    }
+}
